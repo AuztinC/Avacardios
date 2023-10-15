2d4baab8bc5a04a65c34530cab3a06bf31142a27
Madison <maddiewebb188@yahoo.com>
2023-10-14 19:53:25 -0500

    react fix

--- src/Products.js
+++ src/Products.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link,useNavigate,useParams } from 'react-router-dom';
 import Pagination from './Pagination';
 
