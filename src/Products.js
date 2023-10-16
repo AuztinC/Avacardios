380fe60450d402d3130477189b2f25738b79cdb8
Benjamin Herrick <NameinProgressTM@Bens-MacBook-Pro.local>
2023-10-15 19:19:51 -0500

    1 minor fix

--- src/Products.js
+++ src/Products.js
@@ -74,7 +74,7 @@ const Products = ({ products, cartItems, createLineItem, updateLineItem, auth, w
                     product.image ? <img src={product.image}/> : null
                   }
                 </div>
-                { product.name }
+                <Link to={`/products/${product.id}`}>{ product.name }</Link>
                 <div>
                   <p>${product.price.toFixed(2)}</p>
                   <p>Amount: {product.amount}</p>
