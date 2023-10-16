024083441cff9afa8edd22917a7aff2caeb7a4c8
Benjamin Herrick <NameinProgressTM@Bens-MacBook-Pro.local>
2023-10-15 19:14:02 -0500

    you can add products from their individual pages now

--- src/Product.js
+++ src/Product.js
@@ -2,12 +2,14 @@ import React, {useEffect} from "react";
 import {useParams} from "react-router-dom";
 import Reviews from "./Reviews";
 
-const Product =({products,reviews,createReviews,auth})=>{
+const Product =({products,reviews,createReviews,cartItems,updateLineItem,createLineItem,auth})=>{
     const {id}=useParams();
 
     let product=products.find((prod)=>{
         return prod.id===id
     });
+
+    const cartItem = cartItems.find(lineItem => lineItem.product_id === product.id);
 
     useEffect(()=>{
         product=products.find((prod)=>{
@@ -18,8 +20,13 @@ const Product =({products,reviews,createReviews,auth})=>{
         return <h2>Loading...</h2>
     }else{
         return(
-            <div className="product">
+            <div className="products">
                 <h2>{product.name}</h2>
+                {
+                  auth.id ? (
+                    cartItem ? <button onClick={ ()=> updateLineItem(cartItem)}>Add Another</button>: <button onClick={ ()=> createLineItem(product)}>Add</button>
+                  ): null 
+                }<br/>
                 <img src={product.image}/>
                 <h4>${product.price}</h4>
                 <h4>{product.description}</h4>
